424cf25289d6e9ad6ba0e2e3a7ff73b8070c55a3
webinartelran <webinartelran@gmail.com>
2022-11-18 12:34:56 +0200

    Lesson_3

--- src/test/java/StartSelenium.java
+++ src/test/java/StartSelenium.java
@@ -1,8 +1,12 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class StartSelenium {
 
@@ -19,7 +23,16 @@ public class StartSelenium {
     @Test
     public void testName(){
         System.out.println("test started");
+        WebElement element = wd.findElement(By.tagName("a"));
+        List<WebElement> elements = wd.findElements(By.tagName("div"));
+        System.out.println(elements.size());
+        wd.findElement(By.linkText("HOME"));
+        wd.findElement(By.partialLinkText("HO"));
+        wd.findElement(By.id("root"));
+
+
     }
+
 
     @Test
     public void testLogin(){
